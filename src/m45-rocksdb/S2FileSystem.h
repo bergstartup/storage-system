eb6cb8e8daa5bec0cf6fbb04eef024e208285eed
Sudarsan <appsby12@gmail.com>
2022-10-15 16:05:16 +0000

    Conflict fix

--- src/m45-rocksdb/S2FileSystem.h
+++ src/m45-rocksdb/S2FileSystem.h
@@ -32,18 +32,12 @@ SOFTWARE.
 #include <MYFS.h>
 #include <iostream>
 
-<<<<<<< Updated upstream
-namespace ROCKSDB_NAMESPACE {
-
-    class S2FileSystem : public FileSystem {
-=======
 
 
 namespace ROCKSDB_NAMESPACE
 {   
     class S2FileSystem : public FileSystem
     {
->>>>>>> Stashed changes
     public:
         // No copying allowed
         S2FileSystem(std::string uri, bool debug);
@@ -141,10 +135,7 @@ namespace ROCKSDB_NAMESPACE
         struct user_zns_device *_zns_dev;
         std::string _uri;
         const std::string _fs_delimiter = "/";
-<<<<<<< Updated upstream
-=======
         struct MYFS::MYFS_META *FileSystemObj;
->>>>>>> Stashed changes
     };
 }
 
